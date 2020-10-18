3f153b12aaa4f8f021349f5c4f69a96bf4ee1eee
Ari Nykänen <ari.o.nykanen@gmail.com>
2020-10-18 22:36:27 +0300

    Box utility for layout calculations

--- src/boardbuilder.js
+++ src/boardbuilder.js
@@ -4,11 +4,27 @@ var boardbuilder = function() {
 
   const FRAME_WIDTH = 1350;
   const FRAME_HEIGHT = 1080;
+
+  const FRAME_PADDING = 25;
   const FRAME_MARGIN = 50;
+
   const FRAME_4_WIDTH = 2000;
 
   var NamedWidgets = {};
   var WidgetsModified = false;
+
+  const box = (x0,y0, w,h, padding=0) => {
+    const setPadding = (v) => padding = v;
+    const getX = (anchor) => (w-2*padding)*anchor+x0+padding;
+    const getY = (anchor) => (h-2*padding)*anchor+y0+padding;
+    const centerBox = () => [getX(0.5), getY(0.5), w, h];
+    return {
+      setPadding,
+      getX,
+      getY,
+      centerBox,
+    }
+  }
 
   async function build() {
     WidgetsModified = false;
@@ -18,28 +34,33 @@ var boardbuilder = function() {
     if (j) NamedWidgets = await JSON.parse(j);
     console.log(NamedWidgets);
 
+    let boxFrame0 = box(0,0, FRAME_WIDTH,FRAME_HEIGHT, FRAME_PADDING);
+    let boxFrame1 = box((FRAME_WIDTH+FRAME_MARGIN)*1,0, FRAME_WIDTH,FRAME_HEIGHT, FRAME_PADDING);
+    let boxFrame2 = box((FRAME_WIDTH+FRAME_MARGIN)*2,0, FRAME_WIDTH,FRAME_HEIGHT, FRAME_PADDING);
+    let boxFrame3 = box((FRAME_WIDTH+FRAME_MARGIN)*3,0, FRAME_WIDTH,FRAME_HEIGHT, FRAME_PADDING);
+    let boxFrame4 = box((FRAME_WIDTH+FRAME_MARGIN)*3,0, FRAME_4_WIDTH,FRAME_HEIGHT, FRAME_PADDING);
+
     if (!await verifyWidget("Frame0")) {
-      addWidget("Frame0", await createFrame(0,0, FRAME_WIDTH,FRAME_HEIGHT, "Pelaajien nimet"));
+      addWidget("Frame0", await createFrame(...boxFrame0.centerBox(), "Pelaajien nimet"));
     }
 
     if (!await verifyWidget("Frame1")) {
-      addWidget("Frame1", await createFrame( (FRAME_WIDTH+FRAME_MARGIN)*1,0, FRAME_WIDTH,FRAME_HEIGHT, "Me ja skenaarion luonne"));
+      addWidget("Frame1", await createFrame(...boxFrame1.centerBox(), "Me ja skenaarion luonne"));
     }
 
     if (!await verifyWidget("Frame2")) {
-      addWidget("Frame2", await createFrame( (FRAME_WIDTH+FRAME_MARGIN)*2,0, FRAME_WIDTH,FRAME_HEIGHT, "Tulevaisuuden maailma"));
+      addWidget("Frame2", await createFrame(...boxFrame2.centerBox(), "Tulevaisuuden maailma"));
+    }
+    if (!await verifyWidget("DealButton")) {
+      addWidget("DealButton", await createSticker(boxFrame2.getX(0.5), boxFrame2.getY(0.25), "Jaa Ilmiökortti", "#ff9d48"));
     }
 
     if (!await verifyWidget("Frame3")) {
-      addWidget("Frame3", await createFrame( (FRAME_WIDTH+FRAME_MARGIN)*3,0, FRAME_WIDTH,FRAME_HEIGHT, "Me vuonna 20xx"));
+      addWidget("Frame3", await createFrame(...boxFrame3.centerBox(), "Me vuonna 20xx"));
     }
 
     if (!await verifyWidget("Frame4")) {
-      addWidget("Frame4", await createFrame( (FRAME_WIDTH+FRAME_MARGIN)*3+(FRAME_MARGIN+FRAME_WIDTH/2+FRAME_4_WIDTH/2),0, FRAME_4_WIDTH,FRAME_HEIGHT, "Miten tähän päädyttiin?"));
-    }
-
-    if (!await verifyWidget("DealButton")) {
-      addWidget("DealButton", await createSticker((FRAME_WIDTH+FRAME_MARGIN)*2,-380, "Jaa Ilmiökortti", "#ff9d48"));
+      addWidget("Frame4", await createFrame(...boxFrame4.centerBox(), "Miten tähän päädyttiin?"));
     }
 
     if (WidgetsModified) {
