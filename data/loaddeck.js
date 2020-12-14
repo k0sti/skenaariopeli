c9c0dfd888e18c0d5f426f159d335caf8951358d
Ari Nykänen <ari.o.nykanen@gmail.com>
2020-12-14 20:48:55 +0200

    Redirected to original sheet

--- data/loaddeck.js
+++ data/loaddeck.js
@@ -1,8 +1,8 @@
 var ilmiöt = [];
 
 const SPREADSHEET_URL =
-  "1TA9l300L6nobufLbuAeqAaMppQTu1T21WQAJmPRjRG4";
-//  "1qswMijuMmv6wzbPzx8B8IBX3e1-otjM1Hk06KzxhQOE";
+  // "1TA9l300L6nobufLbuAeqAaMppQTu1T21WQAJmPRjRG4"; // YLE's Google account, CORS problem with Google Enterprise account 14.12.2020
+  "1qswMijuMmv6wzbPzx8B8IBX3e1-otjM1Hk06KzxhQOE"; // Ari's Google account, works as of 14.12.2020
 
 (async() => {
   /*await*/ loadSheetData();
